0e56c4a0de49fa661b27f9a9dbece8f98c3fe23b
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-08-24 20:40:06 -0500

    reformatted code with preexisting code

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,45 +1,29 @@
-; Josiah Welch
-; 8/17/25
-; Arca Bootloader
+[org 0x7c00]
+    xor ax, ax      ; Clear ax
+    mov ds, ax      ; Set data segment to 0
+    mov es, ax      ; Set extra segment to 0
+    mov si, version_msg     ; Set si to point to the bootloader version message
+    call print      ; Call print function
+	int 0x21
+    mov si, boot_msg     ; Set si to point to the boot message
+    call print      ; Call print function
+    jmp $           ; Infinite loop
 
-[bits 16] ; Since it boots in real mode
-[org 0x7c00] ; The BIOS loads the code at this address
+print:
+    lodsb           ; Load character from si into al
+    or al, al       ; Check for null terminator
+    jz done         ; If null terminator, jump to done
+    mov ah, 0x0e    ; Set teletype mode
+    int 0x10        ; Call BIOS interrupt to print character
+    jmp print       ; Loop back to print next character
+done:
+    ret
 
-start:
-	mov si, 0 ; Uses si for counter
-	;mov ax, 0x07C0 ; Sets up segment
-	;mov ds, ax
-	;mov es, ax
-	call print
-	call kernel_load
-
-print: ; Sends hello string to screen
-	mov ah, 0x0e
-	mov al, [boot_msg + si] ; Increments a pointer to boot msg
-	int 0x10 ; Interrupt 0x10
-	inc si ; Increments counter
-	cmp byte [boot_msg + si], 0 ; Checks if the counter is at the end of boot msg
-	jne print ; Loops
-
-kernel_load:
-	; Load kernel from disk (sector 2)
-	mov ax, 0x7E00        ; Kernel load address
-	mov es, ax
-	xor bx, bx            ; Buffer offset
-	mov ah, 0x02          ; BIOS read sector
-	mov al, 1             ; Number of sectors
-	mov ch, 0             ; Cylinder
-	mov dh, 0             ; Head
-	mov cl, 2             ; Sector (1=boot, 2=kernel)
-	int 0x13              ; BIOS disk interrupt
-
-	; Jump to kernel
-	jmp 0x7E00
-
-jmp $ ; Creates an infinite loop, which is not ideal...
+version_msg:
+    db "Arca bootloader v0.1", 0x0A, 0 ; Null-terminated message string
 
 boot_msg:
-	db "Arca booting...", 0 ; Boot message initialization
+    db "Arca OS booting...", 0 ; Null-terminated message string
 
-times  510 - ($ - $$) db 0 ; Fills the rest of the 512 byte boot sector with zeroes
-dw 0xAA55 ; Magic number
+times 510-($-$$) db 0 ; Fill the rest of the sector with 0s
+dw 0xaa55 ; Boot signature
